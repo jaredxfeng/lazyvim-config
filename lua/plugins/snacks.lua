d254018b80a4258c020570479eaa5904f6c8db04
jaredxfeng <92383408+jaredxfeng@users.noreply.github.com>
2026-04-09 12:27:50 -0600

    fix: <leader>ff and grep include ignore  files by default

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,32 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      picker = {
+        sources = {
+          files = {
+            exclude = {
+              "**/.git",
+              "**/node_modules",
+            },
+            ignored = true,
+          },
+          grep = {
+            ignored = true,
+            exclude = {
+              "**/.git",
+              "**/node_modules",
+            },
+          },
+          explorer = {
+            hidden = true,
+            ignored = true,
+            exclude = {
+              "**/node_modules",
+              "**/.git",
+            },
+          },
+        },
+      },
       explorer = {
         replace_netrw = false, -- ← this prevents auto-open on startup
       },
